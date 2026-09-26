bed3187a5de5bf5b5e639e4d5dd964f3cc91e459
KayR-Git <kiran.aireddy@gmail.com>
2026-09-26 12:33:16 -0500

    Day7: FindSliceSplit learnigns

--- strings.py
+++ strings.py
@@ -1,8 +1,14 @@
 name = input('Enter your email ID: ')
 domain = name[name.find("@") + 1:]
+l_date = "2026-09-26"
 l_slice = domain[1:4]
 l_split = name.split('.')
+
+l_dateslice = l_date[5:7]
+l_fruits = "apple, Banana, Cherry".split(', ')[1]
 
 print(domain)
 print(l_slice)
 print(l_split)
+print(l_dateslice)
+print(l_fruits)
